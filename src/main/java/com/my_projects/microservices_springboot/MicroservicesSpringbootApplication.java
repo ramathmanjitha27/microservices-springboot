e6102b0287960e1cd5867063d424cf4d5137d610
Ramath Manjitha <ramathmanjitha@gmail.com>
2025-03-30 23:33:33 +0530

    set config

--- src/main/java/com/my_projects/microservices_springboot/MicroservicesSpringbootApplication.java
+++ src/main/java/com/my_projects/microservices_springboot/MicroservicesSpringbootApplication.java
@@ -1,7 +1,9 @@
 package com.my_projects.microservices_springboot;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MicroservicesSpringbootApplication {
@@ -10,4 +12,8 @@ public class MicroservicesSpringbootApplication {
 		SpringApplication.run(MicroservicesSpringbootApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
